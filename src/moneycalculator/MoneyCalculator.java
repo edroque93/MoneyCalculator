1e369eba87326e0fe952471345511216024f9c07
Quique <Quique@10.230.169.43>
2013-10-04 12:01:41 +0100

    Segundo commit, cambios mínimos.

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -3,7 +3,7 @@ package moneycalculator;
 public class MoneyCalculator {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Fin de programa.");
     }
 
 }
