9763c3f32bf07da35a8d7aa8526e9e62056833d2
Quique <Quique@10.230.169.43>
2013-10-04 12:17:45 +0100

    Tercer commit, Currency class agregada al proyecto.

---
+++ src/moneycalculator/Currency.java
@@ -0,0 +1,31 @@
+package moneycalculator;
+
+public class Currency {
+    private String code;
+    private String name;
+    private String symbol;
+
+    public Currency(String code, String name, String symbol) {
+        this.code = code;
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public Currency(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+   
+}
