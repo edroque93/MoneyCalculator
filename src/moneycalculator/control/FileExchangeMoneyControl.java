b7c78f920a3106471c77f96285e25854ed10bd21
Quique <Quique@ThePirateCat>
2014-01-09 00:02:13 +0000

    Pasando a limpio...

---
+++ src/moneycalculator/control/FileExchangeMoneyControl.java
@@ -0,0 +1,51 @@
+package moneycalculator.control;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import moneycalculator.model.Currency;
+import moneycalculator.persistence.FileCurrencySetLoader;
+
+public class FileExchangeMoneyControl {
+
+    private String fileName;
+
+    public FileExchangeMoneyControl(String filename) {
+        this.fileName = filename;
+    }
+
+    public void execute() throws FileNotFoundException {
+        FileCurrencySetLoader.getInstance().load(readCurrencies(fileName));
+    }
+
+    private Currency[] readCurrencies(String filename) throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+        ArrayList<String> listString = new ArrayList<>();
+
+        while (true) {
+            String line;
+            try {
+                line = reader.readLine();
+                if (line == null)
+                    break;
+                listString.add(line);
+            } catch (IOException ioe) {
+                break;
+            }
+        }
+        return fromString(listString);
+    }
+
+    private Currency[] fromString(ArrayList<String> listString) {
+        ArrayList<Currency> listCurrency = new ArrayList<>();
+        for (String string : listString) {
+            String[] split = string.split(";");
+            listCurrency.add(new Currency(split[0], split[1], split[2]));
+        }
+
+        return listCurrency.toArray(new Currency[0]);
+    }
+}
---
+++ src/moneycalculator/ui/swing/PanelMoneyViewer.java
@@ -0,0 +1,32 @@
+package moneycalculator.ui.swing;
+
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import moneycalculator.model.Money;
+import moneycalculator.ui.MoneyViewer;
+
+public class PanelMoneyViewer extends JPanel implements MoneyViewer {
+
+    private JLabel label;
+
+    public PanelMoneyViewer() {
+        super(new FlowLayout(FlowLayout.LEFT));
+        this.createComponents();
+    }
+
+    @Override
+    public void show(Money money) {
+        label.setText(money.toString());
+    }
+
+    private void createComponents() {
+        this.add(createMoneyDisplay());
+    }
+
+    private JLabel createMoneyDisplay() {
+        label = new JLabel();
+        return label;
+    }
+
+}
